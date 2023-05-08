89d7520dedc93e22048e1397abe2bacb6a37c34f
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-08 20:30:10 +0100

    navbar DONE

--- src/Navbar.js
+++ src/Navbar.js
@@ -18,17 +18,25 @@ const NavBar = () => {
                     </Link></li>
                     <li><Link to='/'>Hi {name} !<img src={user}></img></Link></li>
 
-                    <li tabIndex={0}>
-                        <a>
-                            Settings
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                    <div className="dropdown dropdown-end">
+                        <a tabIndex={0} className="btn btn-ghost m-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                         </a>
-                        <ul className="p-2 bg-base-100">
-                            <li><Link to='/'>Profile</Link></li>
-                            <li><Link to='/'>Wallet</Link></li>
-                            <li><Link to='/'>About Us</Link></li>
+                        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Item 1</a></li>
+                            <li><a>Item 2</a></li>
                         </ul>
-                    </li>
+                    </div>
+                    {/* <li tabIndex={0}>
+                        <a>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>                        
+                            </a>
+                        <ul className="p-2 bg-base-100">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li> */}
                 </ul>
             </div>
         </div>
