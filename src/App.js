78d02d75b63fdec823665c42dc953915f5a2e57d
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-08 23:25:51 +0100

    footer Done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './Navbar.js';
 import Home from './Home.js';
+import Footer from './Footer.js';
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <NavBar />
+        <NavBar />
         <div className="content">
           <Routes>
             <Route exact path='/' element={<Home />} />
 
           </Routes>
         </div>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import bandaidImg from './img/band-aid.svg';
+
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-neutral text-neutral-content">
+            <div>
+                <img className="w-20 mb-5" src={bandaidImg} alt="bandaid"></img>
+                <p>DETI Industries Ltd.<br />Providing reliable tech since 2000's</p>
+            </div>
+            <div>
+                <span className="footer-title">Made By:</span>
+                <div className="grid grid-flow-col gap-4">
+                    <ul>
+                        <li>Tiago Mostardinha, 103944</li>
+                        <li>xxx,000</li>
+                        <li>xxx,000</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
