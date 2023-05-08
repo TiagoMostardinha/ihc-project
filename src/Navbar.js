6047d2757b935ec56a7d92a9fefad0d04143c0fb
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-08 20:31:24 +0100

    navbar done

--- src/Navbar.js
+++ src/Navbar.js
@@ -27,16 +27,6 @@ const NavBar = () => {
                             <li><a>Item 2</a></li>
                         </ul>
                     </div>
-                    {/* <li tabIndex={0}>
-                        <a>
-
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>                        
-                            </a>
-                        <ul className="p-2 bg-base-100">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li> */}
                 </ul>
             </div>
         </div>
