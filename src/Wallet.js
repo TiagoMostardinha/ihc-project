fd072ace048685c985088576a840bea37956116d
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-10 01:04:00 +0100

    wtv

--- src/Wallet.js
+++ src/Wallet.js
@@ -1,7 +1,17 @@
+import MyMeds from "./MyMeds.js";
+
 const Wallet = () => {
     return (
-        <div className="navbar bg-base-100">
-            ika
+        <div className="wallet-container mt-10 mb-10">
+            <div className="flex w-full">
+                <div className="grid flex-grow card bg-base-300 rounded-box place-items-center ml-10 mr-5">
+                    <MyMeds />
+                </div>
+                <div className="grid flex-grow card bg-base-300 rounded-box place-items-center ml-5 mr-10">
+                    <MyMeds />
+                </div>
+            </div>
+
         </div>
     );
 }
