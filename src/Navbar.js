5df1ab655c8fdb7d8a5e5069f7f68125b4920a12
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-11 01:07:50 +0100

    navbar dynamic

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,8 +12,8 @@ const NavBar = ({user}) => {
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link to='/'>Log-in</Link></li>
-                    <li><Link to='/'>Sign-in</Link></li>
+                    {!user && <li><Link to='/'>Log-in</Link></li>}
+                    {!user && <li><Link to='/'>Sign-in</Link></li>}
 
                     {user && <li><Link to='/'>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
