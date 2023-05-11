7f777e23e5f6fd2492ecca7c42561f1c62f9f2b6
Pablo Montero Rollán <pabmonrol@alum.us.es>
2023-05-11 09:42:53 +0100

    Sign In fixed | pl-auto

--- src/SignIn.js
+++ src/SignIn.js
@@ -75,65 +75,65 @@ const SignIn = () => {
             <div class="flex justify-center">
                 <h1 className="text-5xl">Sign In</h1>
             </div>
-            <div class="container mx-sm pl-96 pt-16 pb-2">
+            <div class="container mx-sm pl-auto pt-16 pb-2">
                 <h1>Name</h1>
             </div>
-            <div class="container mx-lg flex justify-center pb-4 pl-24">
+            <div class="container mx-lg flex justify-center pb-4 pl-auto">
                 <input type="text" placeholder="" className="input input-bordered input-sm w-3/5" id='uname' />
             </div>
-            <div class="container mx-sm pl-96 pt-4 pb-2">
+            <div class="container mx-sm pl-auto pt-4 pb-2">
                 <h1>E-mail</h1>
             </div>
-            <div class="container mx-lg flex justify-center pb-4 pl-24">
+            <div class="container mx-lg flex justify-center pb-4 pl-auto">
                 <input type="text" placeholder="" className="input input-bordered input-sm w-3/5" id='email' />
             </div>
-            <div class="container mx-sm pl-96 pt-4 pb-2">
+            <div class="container mx-sm pl-auto pt-4 pb-2">
                 <h1>Password</h1>
             </div>
-            <div class="container mx-lg flex justify-center pb-4 pl-24">
+            <div class="container mx-lg flex justify-center pb-4 pl-auto">
                 <input type="password" placeholder="" className="input input-bordered input-sm w-3/5" id='password' />
             </div>
-            <div class="container mx-sm pl-96 pt-4 pb-2">
+            <div class="container mx-sm pl-auto pt-4 pb-2">
                 <h1>Country</h1>
             </div>
-            <div class="container mx-lg flex justify-center pb-4 pl-24"> 
+            <div class="container mx-lg flex justify-center pb-4 pl-auto"> 
                 <input type="text" placeholder="" className="input input-bordered input-sm w-3/5" id='country'/>
             </div>
-            <div class="container mx-sm pl-96 pt-4 pb-2">
+            <div class="container mx-sm pl-auto pt-4 pb-2">
                 <h1>Weight (in kg)</h1>
             </div>
-            <div class="container mx-lg flex justify-center pb-4 pl-24">
+            <div class="container mx-lg flex justify-center pb-4 pl-auto">
                 <input type="text" placeholder="" className="input input-bordered input-sm w-3/5" id='weight'/>
             </div>
-            <div class="container mx-sm pl-96 pt-4 pb-2">
+            <div class="container mx-sm pl-auto pt-4 pb-2">
                 <h1>Height (in cm)</h1>
             </div>
-            <div class="container mx-lg flex justify-center pb-4 pl-24">
+            <div class="container mx-lg flex justify-center pb-4 pl-auto">
                 <input type="text" placeholder="" className="input input-bordered input-sm w-3/5" id='height'/>
             </div>
-            <div class="container mx-sm pl-96 pt-4 pb-2">
+            <div class="container mx-sm pl-auto pt-4 pb-2">
                 <h1>Telephone</h1>
             </div>
-            <div class="container mx-lg flex justify-center pb-4 pl-24">
+            <div class="container mx-lg flex justify-center pb-4 pl-auto">
                 <input type="text" placeholder="" className="input input-bordered input-sm w-3/5" id='telephone'/>
             </div>
-            <div class="container mx-sm pl-96 pt-4 pb-2">
+            <div class="container mx-sm pl-auto pt-4 pb-2">
                 <h1>Allergies</h1>
             </div>
-            <div class="container mx-lg flex justify-center pb-4 pl-24">
+            <div class="container mx-lg flex justify-center pb-4 pl-auto">
                 <input type="text" placeholder="" className="input input-bordered input-sm w-3/5" id='allergies'/>
             </div>
-            <div class="container mx-sm pl-96 pt-4 pb-2">
+            <div class="container mx-sm pl-auto pt-4 pb-2">
                 <h1>Sex</h1>
             </div>
-            <div class="container mx-lg pl-96 pb-4">
+            <div class="container mx-lg pl-auto pb-4">
                 <select class="select select-bordered select-sm w-full max-w-xs" id="genderSelect">
                     <option disabled selected>Select One</option>
                     <option data-value='{"gender":"female"}'>Female</option>
                     <option data-value='{"gender":"male"}'>Male</option>
                 </select>
             </div>
-            <div class="container mx-lg flex justify-center pb-4 pl-24">
+            <div class="container mx-lg flex justify-center pb-4 pl-auto">
                 <Link to='/logged'><button onClick={handleClick} class="btn">Sign In</button></Link>
                 
             </div>
