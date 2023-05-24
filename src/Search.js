f18369b84580e172a7d3851fdae24ff88bbe4076
SlicF <fabiomatias39@ua.pt>
2023-05-24 22:10:04 +0100

    Text Search done avdvanced to do

--- src/Search.js
+++ src/Search.js
@@ -3,15 +3,30 @@ import { Link } from "react-router-dom";
 
 const Search = () => {
     const medicaments = JSON.parse(localStorage.getItem("medicaments"));
+    const [filteredMedicaments, setFilteredMedicaments] = React.useState(medicaments);
+
+    const textSearch = () => {
+        const input = document.getElementById("textSearch");
+
+        if (input.value === "") {
+            setFilteredMedicaments(medicaments);
+            return;
+        }
+        const filter = input.value.toLowerCase();
+        const names = medicaments.map((medicament) => medicament.name);
+        const filteredNames = names.filter((name) => name.toLowerCase().includes(filter));
+        const filteredMedicaments = medicaments.filter((medicament) => filteredNames.includes(medicament.name));
+        setFilteredMedicaments(filteredMedicaments);
+    };
 
     return (
-        <div>
+        <div className="h-screen">
             <div className="flex flex-col items-center space-y-4 py-10">
                 <div className="row search-container flex justify-center items-center">
                     <div className="form-control">
                         <div className="input-group">
-                            <input type="text" placeholder="Search…" className="input input-bordered w-full" />
-                            <button className="btn btn-square">
+                            <input type="text" placeholder="Search…" className="input input-bordered w-full" id="textSearch" onChange={textSearch} />
+                            <button className="btn btn-square" onClick={textSearch}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                             </button>
                         </div>
@@ -77,7 +92,7 @@ const Search = () => {
                 <div className="grid grid-cols-1 gap-4">
                     <div className="medicaments-list">
                         {/* foreach element in medicaments show the element */}
-                        {medicaments.map((medicament) => (
+                        {filteredMedicaments.map((medicament) => (
                             <div className="medicament-item flex items-center justify-between bg-gray-100 p-4" key={medicament.id}>
                                 <div className="flex-grow">
                                     <h2 className="text-2xl font-bold text-gray-800">{medicament.name}</h2>
@@ -90,7 +105,6 @@ const Search = () => {
                 </div>
             </div>
         </div>
-
     );
 }
 
