5fd9950ea2a8c9b9ad5a07912f7ff9db286057a1
SlicF <fabiomatias39@ua.pt>
2023-05-24 20:19:58 +0100

    xota

--- src/medicaments.js
+++ src/medicaments.js
@@ -1,66 +1,75 @@
 const medicaments = [
-    {
-      name: "Paracetamol",
-      description: "Paracetamol is a pain reliever and a fever reducer.",
-      symptoms: ["headache", "muscle aches", "arthritis", "backache", "toothaches", "colds", "fevers"],
-      id: 1,
-    },
-    {
-      name: "Brufen",
-      description: "Brufen is a pain reliever.",
-      symptoms: ["inflammation", "muscle aches", "fevers", "headache"],
-      id: 2,
-    },
-    {
-      name: "Zestril",
-      description: "Lisinopril is the generic drug name for Zestril. It is a blood pressure medication in the angiotensin converting enzyme (ACE) inhibitor group of drugs.",
-      symptoms: ["inflammation", "fevers", "headache"],
-      id: 3,
-    },
-    {
-      name: "Levothyroxine (Synthroid)",
-      description: "This medication is an artificial form of thyroxine (T4), a hormone that comes from the thyroid gland.",
-      symptoms: ["inflammation", "fevers"],
-      id: 4,
-    },
-    {
-      name: "Atorvastatin (Lipitor)",
-      description: "Statin therapies, such as atorvastatin (Lipitor), are the first-line treatment option for high cholesterol.",
-      symptoms: ["inflammation", "muscle aches", "fevers", "headache"],
-      id: 5,
-    },
-    {
-      name: "Metformin (Glucophage)",
-      description: "Metformin (Glucophage) is a diabetes medication.",
-      symptoms: ["inflammation", "fevers"],
-      id: 6,
-    },
-    {
-      name: "Ibuprofen",
-      description: "Ibuprofen is a nonsteroidal anti-inflammatory drug (NSAID) that provides relief from pain, fever, and inflammation.",
-      symptoms: ["headache", "muscle aches", "fevers"],
-      id: 7,
-    },
-    {
-      name: "Amoxicillin",
-      description: "Amoxicillin is an antibiotic used to treat bacterial infections such as respiratory tract infections, urinary tract infections, and skin infections.",
-      symptoms: ["infection", "fevers", "sore throat"],
-      id: 8,
-    },
-    {
-      name: "Loratadine",
-      description: "Loratadine is an antihistamine used to relieve symptoms of allergies such as sneezing, runny nose, and itching.",
-      symptoms: ["allergies", "runny nose", "itching"],
-      id: 9,
-    },
-    {
-      name: "Omeprazole",
-      description: "Omeprazole is a proton pump inhibitor (PPI) used to reduce stomach acid production and treat conditions such as gastroesophageal reflux disease (GERD) and stomach ulcers.",
-      symptoms: ["acid reflux", "stomach ulcers", "indigestion"],
-      id: 10,
-    },
-  ];
-  
+  {
+    name: "Paracetamol",
+    description: "Paracetamol is a pain reliever and a fever reducer.",
+    symptoms: ["headache", "muscle aches", "arthritis", "backache", "toothaches", "colds", "fevers"],
+    use: "Perfect for headache relief",
+    indications: "Always take after each meal",
+    contraindications: "Do not consume if you are pregnant",
+    warning: "The medicine should be stored in a cool and dry place.",
+    adverse_effects: "May cause brief stomach pain in some patients",
+    id: 1,
+  },
+  {
+    name: "Brufen",
+    description: "Brufen is a pain reliever.",
+    symptoms: ["inflammation", "muscle aches", "fevers", "headache"],
+    use: "Effective for reducing inflammation",
+    indications: "Take with food to prevent stomach irritation",
+    contraindications: "Avoid if you have a history of stomach ulcers",
+    warning: "Long-term use can increase the risk of cardiovascular problems.",
+    adverse_effects: "May cause gastrointestinal bleeding in rare cases",
+    id: 2,
+  },
+  {
+    name: "Zestril",
+    description: "Lisinopril is the generic drug name for Zestril. It is a blood pressure medication in the angiotensin converting enzyme (ACE) inhibitor group of drugs.",
+    symptoms: ["inflammation", "fevers", "headache"],
+    use: "Treats high blood pressure and heart failure",
+    indications: "Follow the prescribed dosage and schedule",
+    contraindications: "Not recommended during pregnancy or if you have kidney problems",
+    warning: "Avoid taking potassium supplements while on this medication.",
+    adverse_effects: "May cause dizziness or cough in some patients",
+    id: 3,
+  },
+  {
+    name: "Levothyroxine (Synthroid)",
+    description: "This medication is an artificial form of thyroxine (T4), a hormone that comes from the thyroid gland.",
+    symptoms: ["inflammation", "fevers"],
+    use: "Treats underactive thyroid (hypothyroidism)",
+    indications: "Take on an empty stomach, at least 30 minutes before breakfast",
+    contraindications: "Inform your doctor if you have adrenal gland problems",
+    warning: "Requires regular monitoring of thyroid hormone levels",
+    adverse_effects: "May cause temporary hair loss in some patients",
+    id: 4,
+  },
+  {
+    name: "Atorvastatin (Lipitor)",
+    description: "Statin therapies, such as atorvastatin (Lipitor), are the first-line treatment option for high cholesterol.",
+    symptoms: ["inflammation", "muscle aches", "fevers", "headache"],
+    use: "Lowers cholesterol levels and reduces the risk of cardiovascular events",
+    indications: "Take at bedtime for better absorption",
+    contraindications: "Avoid grapefruit and grapefruit juice while on this medication",
+    warning: "Report any unexplained muscle pain or weakness to your doctor",
+    adverse_effects: "May cause liver enzyme abnormalities in rare cases",
+    id: 5,
+  },
+  {
+    name: "Metformin (Glucophage)",
+    description: "Metformin (Glucophage) is a diabetes medication.",
+    symptoms: ["inflammation", "fevers"],
+    use: "Manages blood sugar levels in type 2 diabetes",
+    indications: "Take with meals to reduce gastrointestinal side effects",
+    contraindications: "Not recommended for individuals with severe kidney or liver disease",
+    warning: "Avoid excessive alcohol consumption while on this medication",
+    adverse_effects: "May cause diarrhea or nausea in some patients",
+    id: 6,
+  },
+  // Add more medicines below...
+];
+
+module.exports = medicaments;
+
 
   localStorage.setItem("medicaments", JSON.stringify(medicaments));
 
